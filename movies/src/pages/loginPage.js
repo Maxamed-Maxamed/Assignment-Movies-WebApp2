ce5bac25f104e5595e2aaf3f29d4dc2f51f5c112
Maxamed Maxamed <20088349@mail.wit.ie>
2023-12-20 20:32:38 +0000

    Refactored login and signup pages validation and adding style

--- movies/src/pages/loginPage.js
+++ movies/src/pages/loginPage.js
@@ -1,21 +1,26 @@
+// // LoginPage.js
 // import React, { useContext, useState } from "react";
 // import { Navigate, useLocation } from "react-router-dom";
 // import { AuthContext } from '../contexts/authContext';
 // import { Link } from "react-router-dom";
+// // import { login } from '../authService'; // Import login function
 
-// const LoginPage = props => {
+
+// const LoginPage = () => {
 //     const context = useContext(AuthContext);
-
 //     const [userName, setUserName] = useState("");
 //     const [password, setPassword] = useState("");
-
 //     const login = () => {
 //         context.authenticate(userName, password);
 //     };
+//     const loginUser = async () => {
+//         const response = await login(userName, password);
+//         // Handle response, set context or show error message
+//         console.log(response); // Log the response for now
+//         // context.authenticate(userName, password);
+//     };
 
 //     let location = useLocation();
-
-//     // Set 'from' to path where browser is redirected after a successful login - either / or the protected path user requested
 //     const { from } = location.state ? { from: location.state.from.pathname } : { from: "/" };
 
 //     if (context.isAuthenticated === true) {
@@ -26,40 +31,44 @@
 //         <>
 //             <h2>Login page</h2>
 //             <p>You must log in to view the protected pages </p>
-//             <input id="username" placeholder="user name" onChange={e => {
-//                 setUserName(e.target.value);
-//             }}></input><br />
-//             <input id="password" type="password" placeholder="password" onChange={e => {
-//                 setPassword(e.target.value);
-//             }}></input><br />
-//             {/* Login web form  */}
-//             <button onClick={login}>Log in</button>
-//             <p>Not Registered?
-//                 <Link to="/signup">Sign Up!</Link></p>
+//             <input
+//                 id="username"
+//                 placeholder="user name"
+//                 onChange={e => setUserName(e.target.value)}
+//             ></input><br />
+//             <input
+//                 id="password"
+//                 type="password"
+//                 placeholder="password"
+//                 onChange={e => setPassword(e.target.value)}
+//             ></input><br />
+//             <button onClick={loginUser}>Log in</button>
+//             <p>
+//                 Not Registered? <Link to="/signup">Sign Up!</Link>
+//             </p>
 //         </>
 //     );
 // };
 
 // export default LoginPage;
 
-// LoginPage.js
 import React, { useContext, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from '../contexts/authContext';
 import { Link } from "react-router-dom";
-// import { login } from '../authService'; // Import login function
-
 
 const LoginPage = () => {
     const context = useContext(AuthContext);
     const [userName, setUserName] = useState("");
     const [password, setPassword] = useState("");
+    
     const login = () => {
         context.authenticate(userName, password);
     };
+    
     const loginUser = async () => {
         const response = await login(userName, password);
-        // Handle response, set context or show error message
+        // Handle response, set context, or show an error message
         console.log(response); // Log the response for now
         // context.authenticate(userName, password);
     };
@@ -72,7 +81,7 @@ const LoginPage = () => {
     }
 
     return (
-        <>
+        <center>
             <h2>Login page</h2>
             <p>You must log in to view the protected pages </p>
             <input
@@ -90,7 +99,7 @@ const LoginPage = () => {
             <p>
                 Not Registered? <Link to="/signup">Sign Up!</Link>
             </p>
-        </>
+        </center>
     );
 };
 
--- movies/src/pages/signUpPage.js
+++ movies/src/pages/signUpPage.js
@@ -1,30 +1,57 @@
+// export default SignUpPage;
 import React, { useContext, useState } from "react";
 import { Navigate } from "react-router-dom";
 import { AuthContext } from '../contexts/authContext';
 
 const SignUpPage = props => {
-  const context = useContext(AuthContext)
+  const context = useContext(AuthContext);
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const [passwordAgain, setPasswordAgain] = useState("");
   const [registered, setRegistered] = useState(false);
+  const [error, setError] = useState("");
+  // const register = () => {
+  //   let passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  //   const validPassword = passwordRegEx.test(password);
+
+  //   if (validPassword && password === passwordAgain) {
+  //     context.register(userName, password);
+  //     setRegistered(true);
+  //   }
+  // }
+
+  // if (registered === true) {
+  //   return <Navigate to="/login" />;
+  // }
+
+  
 
   const register = () => {
     let passwordRegEx = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
     const validPassword = passwordRegEx.test(password);
 
     if (validPassword && password === passwordAgain) {
-      context.register(userName, password);
-      setRegistered(true);
+      try {
+        // Assuming context.register returns a Promise
+        context.register(userName, password).then(() => {
+          setRegistered(true);
+        }).catch(() => {
+          setError("Registration failed. Please try again.");
+        });
+      } catch (error) {
+        setError("Registration failed. Please try again.");
+      }
+    } else {
+      setError("Invalid password or passwords do not match.");
     }
-  }
+  };
 
   if (registered === true) {
     return <Navigate to="/login" />;
   }
 
   return (
-    <>
+    <center>
       <h2>SignUp page</h2>
       <p>You must register a username and password to log in </p>
       <input value={userName} placeholder="user name" onChange={e => {
@@ -37,8 +64,9 @@ const SignUpPage = props => {
         setPasswordAgain(e.target.value);
       }}></input><br />
       {/* Login web form  */}
+      {error && <p style={{color: "red"}}>{error}</p>}
       <button onClick={register}>Register</button>
-    </>
+    </center>
   );
 };
 
